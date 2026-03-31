e86b48ce71e2d7b25ab76a8892a673181f7c1553
Gislaine Silva <silvagi@delawareconsulting.com>
2026-03-31 15:01:29 -0300

    fix: botoes de controle de voltar/avancar do calendario flutuando fora do lugar

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -19,7 +19,7 @@ function Calendar({
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
-      className={cn("p-3", className)}
+      className={cn("p-3 relative", className)}
       locale={ptBR}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
